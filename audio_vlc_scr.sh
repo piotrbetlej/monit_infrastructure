1426aeeb5a1d41d242780e0fc1ddf22818ebd5bc
Piotr Betlej <piotr.betlej@googlemail.com>
2016-01-17 17:46:39 +0100

    Update audio_vlc_scr.sh

--- audio_vlc_scr.sh
+++ audio_vlc_scr.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-/bin/su piob -c "cvlc -vvv alsa:\/\/hw:1,0 --sout \"#transcode{acodec=flac,ab=32}:standard{access=http,mux=ogg,dst=:8082}\""&
-echo $! > /tmp/audio_vlc.pid
+#if ps -A | grep -q "vlca";  then
+#        /bin/su root -c "killall vlca"
+#fi
 
-/bin/bash -c 'kill -s SIGTERM `cat /tmp/audio_vlc.pid` && rm /tmp/audio_vlc*'
+# Must be vlca -I "dummy" here since cvlc is only a script running vlc with dummy interface.Renaming this script does nothing.Vlc must be symlinked to use it's new 
+# name with killall.
+/bin/su piob -c "vlca -I "dummy" alsa:\/\/hw:1,0 vlc://quit --sout \"#transcode{acodec=flac,ab=32}:standard{access=http,mux=ogg,dst=:8082}\"" &
 
-/bin/su piob -c "cvlc -vvv alsa:\/\/hw:1,0 --sout \"#transcode{acodec=flac,ab=32}:standard{access=http,mux=ogg,dst=:8082}\""&
-echo $! > /tmp/audio_vlc.pid
+#/bin/su root -c "killall vlca"
+#/bin/su piob -c "vlca -I "dummy" alsa:\/\/hw:1,0 --sout \"#transcode{acodec=flac,ab=32}:standard{access=http,mux=ogg,dst=:8082}\"" &
 
+# echo $! > /tmp/audio_vlc.pid
+
+# /bin/bash -c 'kill -s SIGTERM `cat /tmp/audio_vlc.pid` && rm /tmp/audio_vlc*'
+
+# /bin/su piob -c "cvlc -vvv alsa:\/\/hw:1,0 --sout \"#transcode{acodec=flac,ab=32}:standard{access=http,mux=ogg,dst=:8082}\""&
+# echo $! > /tmp/audio_vlc.pid
