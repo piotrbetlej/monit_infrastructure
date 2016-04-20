8f90fc86745bf1defb13377675d16c84da502104
Piotr Betlej <piotr.betlej@googlemail.com>
2016-04-20 14:30:38 +0200

    Update video_vlc_scr.sh

--- video_vlc_scr.sh
+++ video_vlc_scr.sh
@@ -4,12 +4,12 @@
 #fi
 # Must be vlca -I "dummy" here since cvlc is only a script running vlc with dummy interface.Renaming this script does nothing.Vlc must be symlinked to use it's new 
 # name with killall.
-/bin/su piob -c "vlcv -I "dummy" v4l2:\/\/\/dev\/video0:chroma=mjpg:width=1280:height=720 vlc://quit --sout \"#standard{access=http,mux=mpjpeg,dst=:8083}\"" &
+/bin/su user -c "vlcv -I "dummy" v4l2:\/\/\/dev\/video0:chroma=mjpg:width=1280:height=720 vlc://quit --sout \"#standard{access=http,mux=mpjpeg,dst=:8083}\"" &
 
 # echo $! > /tmp/video_vlc.pid
 
 # /bin/bash -c 'kill -s SIGTERM `cat /tmp/video_vlc.pid` && rm /tmp/video_vlc*'
 
-# /bin/su piob -c "cvlc v4l2:\/\/\/dev\/video0:chroma=mjpg:width=1280:height=720 --sout \"#standard{access=http,mux=mpjpeg,dst=:8081}\""&
+# /bin/su user -c "cvlc v4l2:\/\/\/dev\/video0:chroma=mjpg:width=1280:height=720 --sout \"#standard{access=http,mux=mpjpeg,dst=:8081}\""&
 # echo $! > /tmp/video_vlc.pid
 
